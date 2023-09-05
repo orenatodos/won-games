f3c0fae17b411061c12d465bcbd2849e8e7c99bf
Renato Silva <renatosilvacode@gmail.com>
2023-09-04 21:25:45 -0300

    feat: initial setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
